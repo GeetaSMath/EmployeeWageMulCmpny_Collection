a91acd614e7acdd37beb7c37b253356bd0231f76
GeetaSMath <geetasmath@gmail.com>
2021-10-26 10:48:18 -0600

    Added: Welcome to Solve EmployeeWagepRoblem Refactor by collection

---
+++ src/com/bridgelabz/EmployeeWageRefactorCollection.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeeWageRefactorCollection {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Problem with Multiple Compnies Using Collection");
+    }
+}
