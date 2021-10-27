c71a19ecf67b54753d18078723917204c87d2f4e
GeetaSMath <geetasmath@gmail.com>
2021-10-26 21:08:28 -0600

    Added: Ability To Mange Multiple Companies

--- src/com/bridgelabz/EmployeeWageRefactorCollection.java
+++ src/com/bridgelabz/EmployeeWageRefactorCollection.java
@@ -3,79 +3,73 @@ import java.util.Random;
 
  class EmployeeWageRefactorCollection {
 
-        private static final int FULLDAYHR = 8, PARTTIMEHR = 4;
-        private static int totalMonthWage = 0;
-        static int checkEmp;
-        static int totalHrCount = 0;
+         public String company;
+         public int wagePerHr;
+         public int maxMonthHr;
+         public int workingDays;
+         public int fullTimeHr;
+         public int partTimeHr;
 
-        private int wageHr;
-        private int workDaysLimit;
-        private int hrLimit;
+         public EmployeeWageRefactorCollection(String company, int wagePerHr, int maxMonthHr, int workingDays, int fullTimeHr, int partTimeHr) {
+             this.company = company;
+             this.wagePerHr = wagePerHr;
+             this.maxMonthHr = maxMonthHr;
+             this.workingDays = workingDays;
+             this.fullTimeHr = fullTimeHr;
+             this.partTimeHr = partTimeHr;
+         }
+     }
 
-        public void setVariables(int x, int y, int z){
-            this.wageHr = x;
-            this.workDaysLimit = y;
-            this.hrLimit = z;
-        }
+     class EmpWageArry {
+         private static final int IS_FULL_TIME = 1;
+         private static final int IS_PART_TIME = 2;
 
-        public static void setCheckEmp(){
-            Random rdm = new Random();
-            int rd = rdm.nextInt(3);
-            checkEmp = rd;
-        }
+         private int numOfCmpny=0;
+         private EmployeeWageRefactorCollection[] compnyWageArray;
 
-        public void  setTotalMonthWage(){
-            int day=0;
-            while (workDaysLimit>0){
-                setCheckEmp();
-                switch (checkEmp){
-                    case 1:
-                        totalMonthWage = totalMonthWage + (wageHr * FULLDAYHR);
-                        totalHrCount +=  FULLDAYHR;
-                        workDaysLimit--;
-                        day++;
-                        System.out.println("Day:"+day+"  Wage : "+totalMonthWage+" Working Hrs : "+totalHrCount);
-                        break;
-                    case 2:
-                        totalMonthWage = totalMonthWage + (wageHr * PARTTIMEHR);
-                        totalHrCount += PARTTIMEHR;
-                        workDaysLimit--;
-                        day++;
-                        System.out.println("Day:"+day+"  Wage : "+totalMonthWage+" Working Hrs : "+totalHrCount);
-                    case 3:
-                        workDaysLimit--;
-                        day++;
-                        System.out.print("Day:"+day+"  Wage : "+totalMonthWage+" Working Hrs : "+totalHrCount);
-                        System.out.println("<---Employee was Absent");
-                    default:
-                        if (totalHrCount == hrLimit)
-                            break;
+         public EmpWageArry(){
+             compnyWageArray = new EmployeeWageRefactorCollection[5];
+         }
 
-                }
+         public void addCompanyEmpWage(String company, int wagePerHr, int maxMonthHr, int workingDays, int fullTimeHr, int partTimeHr) {
+             compnyWageArray[numOfCmpny] = new EmployeeWageRefactorCollection(company,wagePerHr,maxMonthHr,workingDays,fullTimeHr,partTimeHr);
+             computeEmpWage(compnyWageArray[numOfCmpny].company, compnyWageArray[numOfCmpny].wagePerHr
+                     ,compnyWageArray[numOfCmpny].maxMonthHr ,compnyWageArray[numOfCmpny].workingDays
+                     ,compnyWageArray[numOfCmpny].fullTimeHr,compnyWageArray[numOfCmpny].partTimeHr);
+             numOfCmpny++;
+         }
 
-            }
-            System.out.println("\nTotal Wage of the Month : "+totalMonthWage);
-            System.out.println("Total Hrs of the Employee : "+totalHrCount);
+         public static void computeEmpWage(String company, int wagePerHr, int maxMonthHr, int workingDays, int fullTimeHr, int partTimeHr){
+             int empHr = 0;
+             int totalEmpHr = 0;
+             int days = 0;
 
-        }
-    }
+             while (empHr <= maxMonthHr && days <= workingDays) {
 
+                 double empCheck = Math.floor(Math.random() * 10) % 3;
+                 int empCheck1 = (int) empCheck;
+
+                 switch (empCheck1) {
+                     case IS_FULL_TIME:
+                         empHr = fullTimeHr;
+                         break;
+                     case IS_PART_TIME:
+                         empHr = partTimeHr;
+                         break;
+                     default:
+                         empHr = 0;
+                 }
+                 totalEmpHr = totalEmpHr + empHr;
+                 days++;
+             }
+             int totalWage = totalEmpHr * wagePerHr;
+             System.out.println("Total Wage For " + company + " Company Employee Is " + totalWage);
+         }
+     }
      class Emp {
-     public static void main(String[] args) {
-
-            EmployeeWageRefactorCollection ibm = new EmployeeWageRefactorCollection();
-            System.out.println("Employee Wage for 'IBM': ");
-            ibm.setVariables(450, 25, 150);
-            ibm.setTotalMonthWage();
-
-            EmployeeWageRefactorCollection tcs = new EmployeeWageRefactorCollection();
-            System.out.println("Employee Wage for 'TCS': ");
-            tcs.setVariables(380, 23, 150);
-            tcs.setTotalMonthWage();
-
-            EmployeeWageRefactorCollection hcl = new EmployeeWageRefactorCollection();
-            System.out.println("Employee Wage for 'HCL': ");
-            hcl.setVariables(320, 20, 150);
-            hcl.setTotalMonthWage();
-        }
-    }
+         public static void main(String[] args) {
+             EmpWageArry obj = new EmpWageArry();
+             obj.addCompanyEmpWage("TCS", 10, 50, 20, 8, 4);
+             obj.addCompanyEmpWage("D-mart", 20, 50, 20, 6, 3);
+         }
+     }
